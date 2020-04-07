601ffc9f8759a0fbaa102e03c976816d76acd8d0
cds-group <62610620+cds-group@users.noreply.github.com>
2020-04-07 18:13:29 +0200

    Add files via upload

---
+++ R/getMetGraph.R
@@ -0,0 +1,42 @@
+#' Transform the edges list of each sample in graphml format graphs
+#'
+#' This function create a graph for each sample where metabolites are nodes and the enzymes are the edges, whose weight is given by the expression value.
+#' It simplifies the multiedges in single by taking the mean of enzyme catalyzing the same reaction and summing up those coming from different reactions.
+#' @param input_dir path to input directory containing the samples edge lists
+#' @param output_dir path to output directory
+#' @return graphs in graphml format
+#' @import igraph
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' input_dir <- system.file("extdata/edges_list/", package = "Met2Graph"))
+#' output_dir <- "./"
+#' getMetGraph(input_dir,output_dir)}
+getMetGraph <- function(input_dir, output_dir){
+  fileListKeep <- gsub('.txt', '', list.files(input_dir))
+  for (i in fileListKeep){
+    fileNamefull <- file.path(input_dir, paste0(i, ".txt"))
+    graphRead <- read.csv(fileNamefull, header = TRUE)
+  # Run for finding the mean of same rkns and sum for different rkns----------
+    graphRead$met_nodes <- paste(graphRead$met1, graphRead$met2,sep = "_")
+    graphRead$HMR_met_nodes <- paste(graphRead$Reaction, graphRead$met_nodes ,sep = "_")
+
+  graph_mean <- as.data.frame(tapply(graphRead$expr, graphRead$HMR_met_nodes, mean))
+  graph_mean$nodes <- substring(rownames(graph_mean), 10)
+  graph_mean_sum <- as.data.frame(tapply(graph_mean$`tapply(graphRead$expr, graphRead$HMR_met_nodes, mean)`, graph_mean$nodes, sum))
+
+  options(stringsAsFactors = FALSE)
+  inpGraph <- as.data.frame(sub('\\_.*', '', row.names(graph_mean_sum)))
+  colnames(inpGraph) <- "V1"
+  inpGraph$V2 <- sub('.*\\_', '',row.names(graph_mean_sum) )
+  inpGraph$weight <- graph_mean_sum[, 1]
+  colnames(inpGraph) <- c("V1", "V2", "weight")
+
+  graphMet <- graph.data.frame(inpGraph, directed = TRUE)
+
+  fileNameout <- file.path(output_dir, paste0(i, '.graphml'))
+  write.graph(graphMet, fileNameout, "graphml")
+
+}
+}
---
+++ R/integrate_expression.R
@@ -0,0 +1,66 @@
+#' Add expression values to enzymes catalyzing each reaction for each sample
+#'
+#' This function create a new file for each sample containing reactions, metabolites connected, enzymes and expression values
+#' @param df dataframe of edges list containing, reactions. metabolites and enzymes. The latter named "Enzyme"
+#' @param expr_dir path to directory containing samples expression data
+#' @param extension files extension
+#' @param tag new filename tags
+#' @param output_dir path to output directory
+#' @return Sample specific dataframes with reactions, metabolites edges, enzymes and expression values
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' expr_dir <- system.file("extdata/expression/", package = "Met2Graph"))
+#' output_dir <- "./"
+#' integrate_expression(edges_list_no_rec_mets,expr_dir, "txt", "edges-", output_dir)}
+integrate_expression <- function(df, expr_dir, extension, tag, output_dir){
+    if(missing(expr_dir)) {
+      setwd("./")
+    } else {
+      setwd(expr_dir)
+    }
+
+  if(missing(extension)) {
+    extension="txt"
+  } else {
+    extension=extension
+  }
+  if(missing(tag)) {
+    tag="edges-"
+  } else {
+    tag=tag
+  }
+  fileNames <- Sys.glob("*")
+
+  fileNumbers <- seq(fileNames)
+
+  for (fileNumber in fileNumbers) {
+
+    newFileName <-  paste(tag,
+                          sub(paste("\\.", extension, sep = ""), "", fileNames[fileNumber]),
+                          ".", extension, sep = "")
+
+    # read expression data:
+    sample <- read.table(fileNames[fileNumber],
+                         header = TRUE, row.names = NULL)
+
+
+    colnames(sample)[1]="gene"
+    colnames(sample)[2]="v1"
+
+    df$expr= sample$v1[match(df$Enzyme,sample$gene)]
+    f4=df[!(is.na(df$expr) | df$expr=="NA"), ]
+    # write old data to new files:
+    write.table(f4,
+                paste0(output_dir,newFileName),
+                append = FALSE,
+                quote = FALSE,
+                sep = ",",
+                row.names = FALSE,
+                col.names = TRUE)
+
+  }
+
+}
